7ecf99f5ed14f5585ad9322c37a6f5184bbb3252
Anna Jakubovskaja <anna28.78@bk.ru>
2022-11-22 04:24:45 +0300

    Task2 solution

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -6,38 +6,16 @@
 
 //32679 -> 6
 
-Console.WriteLine("Введите число: ");
-int x=Convert.ToInt32(Console.ReadLine());
-int n=0;
+Console.Write("Введите число: ");
+int num=Convert.ToInt32(Console.ReadLine());
 
-while (x>0);
+string str = num.ToString();
+if (num<100)
 {
-  n++;
-  x=x/10;
-
+  Console.WriteLine ("Третей цифры нет");
+}
+else
+{
+Console.WriteLine(str[2]);
 }
 
-//int array [n]
-//int number = x;
-//for(int i=n-1; i>=0; i--);
-//{
-   // array[i] = number %10;
-  //  number/=10; 
-//}
-
-//if(n>=3)
-//{
-  //  Console.WriteLine(array[2]);
-//}
-//else
-//{
-  //  Console.WriteLine("Третей цифры нет!");
-//}
-
-if(n>99)
-{
-  Console.WriteLine((n/100)%1);
-}
-
-
-//Console.WriteLine();
